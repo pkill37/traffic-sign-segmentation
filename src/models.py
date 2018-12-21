91188472f0cd72eaf56253bf76808794d717a564
Fábio Maia <fabiu.maia@gmail.com>
2018-12-22 00:38:00 +0100

    Rename output_neurons to output_units for consistency

--- src/models.py
+++ src/models.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 
 
-def vgg16(output_neurons, output_activation, nb_layers=None):
+def vgg16(output_units, output_activation, nb_layers=None):
     # Freeze the model's first n weights
     vgg16 = tf.keras.applications.vgg16.VGG16(weights='imagenet')
     for layer in vgg16.layers[:nb_layers]:
@@ -9,7 +9,7 @@ def vgg16(output_neurons, output_activation, nb_layers=None):
 
     # Stitch VGG16 to our own fully-connected layer for segmentation
     x = vgg16.get_layer('fc2').output
-    x = tf.keras.layers.Dense(output_neurons, name='pilita', activation=output_activation)(x)
+    x = tf.keras.layers.Dense(units=output_units, activation=output_activation)(x)
     x = tf.keras.layers.Reshape((224, 224, 1))(x)
     model = tf.keras.models.Model(inputs=vgg16.input, outputs=x)
     model.summary()

dc06bd547fad160f3be563080d5e431ce929370d
Fábio Maia <fabiu.maia@gmail.com>
2018-12-26 23:22:14 +0100

    Add test script

--- src/test.py
+++ src/test.py
@@ -1,7 +1,35 @@
-def predict_one():
-    image_batch, mask_batch = next(validation_generator)
-    predicted_mask_batch = model.predict(image_batch)
-    image = image_batch[0]
-    predicted_mask = predicted_mask_batch[0].reshape(SIZE)
-    plt.imshow(image)
-    plt.imshow(predicted_mask, alpha=0.6)
+import os
+import argparse
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import numpy as np
+import data
+
+
+if __name__ == '__main__':
+    pwd = os.path.realpath(__file__)
+    out_dir = os.path.abspath(os.path.join(pwd, '../../out/')) + '/'
+    data_dir = os.path.abspath(os.path.join(pwd, '../../data/')) + '/'
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model', type=str, default=out_dir+'models/best.hdf5')
+    parser.add_argument('--images_path', type=str, default=data_dir+'images/')
+    parser.add_argument('--labels_path', type=str, default=data_dir+'labels/')
+    parser.add_argument('--img_height', type=int, default=224)
+    parser.add_argument('--img_width', type=int, default=224)
+    parser.add_argument('--batch_size', type=int, default=1)
+    args = parser.parse_args()
+
+    model = tf.keras.models.load_model(args.model)
+
+    plt.ion()
+
+    for x_batch, y_batch in data.MaskedImageSequence(images_path=args.images_path, labels_path=args.labels_path, img_height=args.img_height, img_width=args.img_width, batch_size=args.batch_size, augment=False):
+        y_pred = model.predict_on_batch(x_batch)
+        #y_pred = (y_pred > 0.5).astype(np.uint8)
+
+        for i in range(len(x_batch)):
+            plt.imshow(x_batch[i,:,:,:])
+            plt.imshow(y_pred[i,:,:,0], alpha=0.7)
+            input('Press [Enter] to predict another mini-batch...')
+            plt.close()

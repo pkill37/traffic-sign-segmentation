8f87684cb45ac2e59a4b1efd66695af5d35413ab
Fábio Maia <fabiu.maia@gmail.com>
2019-01-10 20:08:57 +0100

    Cleanup helpers

--- src/helpers.py
+++ src/helpers.py
@@ -1,8 +1,3 @@
-import os
-import matplotlib.pyplot as plt
-import tensorflow as tf
-
-
 def seed():
     from random import seed
     seed(1)
@@ -10,27 +5,3 @@ def seed():
     numpy.random.seed(2)
     from tensorflow import set_random_seed
     set_random_seed(3)
-
-
-def get_parent_path(path):
-    return os.path.abspath(os.path.join(path, '..')) + '/'
-
-
-def plot_metrics(history):
-    # Plot training & validation accuracy values
-    plt.plot(history.history['acc'])
-    plt.plot(history.history['val_acc'])
-    plt.title('Model accuracy')
-    plt.ylabel('Accuracy')
-    plt.xlabel('Epoch')
-    plt.legend(['Train', 'Test'], loc='upper left')
-    plt.show()
-
-    # Plot training & validation loss values
-    plt.plot(history.history['loss'])
-    plt.plot(history.history['val_loss'])
-    plt.title('Model loss')
-    plt.ylabel('Loss')
-    plt.xlabel('Epoch')
-    plt.legend(['Train', 'Test'], loc='upper left')
-    plt.show()

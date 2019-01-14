f03699048938120180c2815c65e37f9ae079382d
Fábio Maia <fabiu.maia@gmail.com>
2019-01-14 16:53:24 +0100

    Load data in memory and specify a simple validation split

--- src/train.py
+++ src/train.py
@@ -23,8 +23,8 @@ if __name__ == '__main__':
     parser.add_argument('--labels_path', type=str, default=data_dir+'labels/')
     parser.add_argument('--img_height', type=int, default=224)
     parser.add_argument('--img_width', type=int, default=224)
-    parser.add_argument('--epochs', type=int, default=3)
-    parser.add_argument('--batch_size', type=int, default=32)
+    parser.add_argument('--epochs', type=int, default=100)
+    parser.add_argument('--batch_size', type=int, default=16)
     parser.add_argument('--optimizer', type=str, default='adam')
     parser.add_argument('--output_activation', type=str, default='relu')
     parser.add_argument('--learning_rate', type=float, default=0.01)
@@ -35,20 +35,23 @@ if __name__ == '__main__':
     model.summary()
 
     callbacks = [
-        tf.keras.callbacks.EarlyStopping(monitor='loss', min_delta=0, patience=0, verbose=0, mode='auto', baseline=None),
+        tf.keras.callbacks.EarlyStopping(monitor='val_acc', min_delta=0, patience=0, verbose=0, mode='auto', baseline=None),
         tf.keras.callbacks.LearningRateScheduler(lambda epoch: args.learning_rate*(0.1**int(epoch/10))),
-        tf.keras.callbacks.ReduceLROnPlateau(monitor='loss', factor=0.2, patience=10, verbose=0, mode='auto', min_delta=0.0001, cooldown=0, min_lr=0.001),
-        tf.keras.callbacks.ModelCheckpoint(filepath=args.model, monitor='loss', verbose=1, save_best_only=True, save_weights_only=False, mode='auto', period=1),
+        tf.keras.callbacks.ReduceLROnPlateau(monitor='val_acc', factor=0.2, patience=10, verbose=0, mode='auto', min_delta=0.0001, cooldown=0, min_lr=0.001),
+        tf.keras.callbacks.ModelCheckpoint(filepath=args.model, monitor='val_acc', verbose=1, save_best_only=True, save_weights_only=False, mode='auto', period=1),
         tf.keras.callbacks.TensorBoard(log_dir=args.tensorboard, histogram_freq=0, write_graph=True, write_images=True),
         tf.keras.callbacks.CSVLogger(filename=args.log, separator=',', append=False),
     ]
 
-    model.fit_generator(
-        generator=data.MaskedImageSequence(images_path=args.images_path, labels_path=args.labels_path, img_height=args.img_height, img_width=args.img_width, batch_size=args.batch_size),
+    x, y = data.load_data(images=data.list_pictures(args.images_path), labels=data.list_pictures(args.labels_path), img_height=args.img_height, img_width=args.img_width)
+
+    model.fit(
+        x=x,
+        y=y,
+        batch_size=args.batch_size,
         epochs=args.epochs,
-        verbose=1,
+        validation_split=0.2,
         shuffle=True,
+        verbose=1,
         callbacks=callbacks,
-        workers=multiprocessing.cpu_count()-1 or 1,
-        use_multiprocessing=True,
     )
